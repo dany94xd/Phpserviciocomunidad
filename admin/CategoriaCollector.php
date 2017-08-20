1c5bba837940cc3955e9030d597fbb8d8e45cb98
dany94xd <danimaxi60@hotmail.com>
2017-08-20 10:08:17 -0500

    collector de categoria

---
+++ admin/CategoriaCollector.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once('Categoria.php');
+include_once('Collector.php');
+
+class CategoriaCollector extends Collector
+{
+  
+  function showCategorias() {
+    $rows = self::$db->getRows("SELECT * FROM comunidad ");        
+    echo "linea 1";
+    $arrayDemo= array();        
+    foreach ($rows as $c){
+      $aux = new Demo($c{'id_categoria'},$c{'nombre'});
+      array_push($arrayDemo, $aux);
+    }
+    return $arrayDemo;        
+  }
+function showCategoria($id) {
+    $rows = self::$db->getRows("SELECT * FROM comunidad where id_categoria= ? ", array ("{$id}"));        
+$ObjDemo= new Demo($rows[0]{'iddemo'},$rows[0]{'nombre'});
+    
+    return $ObjDemo;        
+  }
+function UpdateCategoria($id,$nombre) {
+    $insertrow = self::$db->updateRow("UPDATE public.Categoria SET nombre = ? where id_categoria= ? ", array ("{$nombre}",$id));
+
+}
+function deleteCategoria($id) {
+    $deleterow = self::$db->deleteRow("DELETE FROM public.Categoria where id_categoria= ? ", array ("{$id}"));
+
+}
+
+
+}
+?>
