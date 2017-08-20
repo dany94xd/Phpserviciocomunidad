c71a9d5d1c56b8d5456f962aaa86862ad1572acf
dany94xd <danimaxi60@hotmail.com>
2017-08-20 10:18:54 -0500

    clase de la tabla categoria

---
+++ admin/Categoria.php
@@ -0,0 +1,27 @@
+<?php
+
+class Categoria
+{
+    private $id_categoria;
+    private $nombre;
+    
+     function __construct($id_categoria, $nombre) {
+       $this->id_categoria = $id_categoria;
+       $this->nombre = $nombre;
+     }
+    
+     function setIdcategoria($id_categoria){
+       $this->id_categoria = $id_categoria;
+     } 
+     function getcategoria(){
+       return $this->id_categoria;
+     } 
+     function setNombre($nombre){
+       $this->nombre = $nombre;
+     } 
+     function getNombre(){
+       return $this->nombre;
+     } 
+}
+
+?> 
