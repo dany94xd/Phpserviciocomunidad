03d4c82f2cec3f2ecda79557cc0c3565b74d0979
dany94xd <danimaxi60@hotmail.com>
2017-08-27 17:22:16 -0500

    crud de tabla estado de denuncia

--- admin/updateEstadoDenuncia.php
+++ admin/updateEstadoDenuncia.php
@@ -6,7 +6,12 @@ session_start();
 <html lang="es">
 	<head>
 		<meta charset ="utf-8">
-		<title> actualizar EStado DEnuncia</title>
+		<title> Actualizar EstadoDenucia </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <link href="../css/tablas.css" rel="stylesheet" >
 	</head>
 <body>
 
@@ -17,19 +22,57 @@ session_start();
 </section>
 <aside>
 <?php
-$nombre=$_POST["nombre"];
-$id_estado_denuncia=$_POST["id_estado_denuncia"];
+
+echo "<nav class='navbar navbar-default'>";
+    echo "<div class='container-fluid'>";
+    echo "<div class='navbar-header'><a class='navbar-brand' >Tabla EstadoDenucia</a></div>";
+    echo " <ul class='nav navbar-nav'>";
+		      	echo "<li><a href='readsupremo.php'>Menú</a></li>";
+			echo "<li><a href='crearestadodenuncia.php'>Nuevo</a></li>";
+		echo "</ul>";
+    echo " <ul class='nav navbar-nav navbar-right'>";
+    echo "<li><a href='#'>Hola Usuario : (" . $_SESSION ['MiSession'] . ")</a></li>";
+    echo "<li><a href='salir.php''><span class='glyphicon glyphicon-log-in'></span> Salir</a></li>";
+    echo "</ul>";
+    echo "</div>";
+    echo "</nav>";
+
+
+
+$id=$_GET['id'];
+$nombre=$_GET['nombre'];
+
+
+
 echo "Edicion en proceso . . . . </br>";
-include_once("EstadoDenunciaCollector.php");
-$EstadoDenunciaCollectorObj= new EstadoDenunciaCollector();
-$EstadoDenunciaCollectorObj->updateEstadoDenuncia($id_estado_denuncia,$nombre);
-echo "id: ".$id_estado_denuncia." actualizado a:".$nombre." </br>";
+
 ?>
 
-<div class="text-fieldsl">
-         <a href='index.php'>Volver al inicio</a>                                    
-</div>
+<form method= "POST" class="form-horizontal" action= "GuardarEstadoDenuncia.php?usuario=" >
+     <div class="form-group">
+         <label for="inputName" class="control-label col-xs-2">Código:</label>
+         <div class="col-xs-10">
+             <input name = "Codigo" type="text" id= "Codigo" class="form-control"
+ placeholder="Codigo" value="<?php echo $id;?>" readonly>
+         </div>
+     </div>
+     <div class="form-group">
+         <label for="inputName" class="control-label col-xs-2">Nombre:</label>
+         <div class="col-xs-10">
+             <input name = "Nombre" type="text" id= "Nombre" class="form-control" placeholder="Nombre" value="<?php echo $nombre;?>">
+         </div>
+     </div>
+     <div class="form-group">
+         <div class="col-xs-offset-2 col-xs-10">
+             <button type="submit" class="btn btn-primary">Grabar</button>
+         </div>
+     </div>
+</form>
+
+
 
 </aside>
 </body>
+</html>
+
 </html>
