64067173a8b9f1abe93bc9c531020b314f34687b
Olga Tohabanda <olgseb@gmail.com>
2017-08-27 22:33:24 -0500

    modificacion de ProvinciaCollector

--- admin/ProvinciaCollector.php
+++ admin/ProvinciaCollector.php
@@ -6,31 +6,34 @@ include_once('Collector.php');
 class ProvinciaCollector extends Collector
 {
   
-  function showProvincias() {
-    $rows = self::$db->getRows("SELECT * FROM provincia ");        
-    echo "linea 1";
-    $arrayDemo= array();        
+function showProvincias() {
+   $rows = self::$db->getRows("SELECT * FROM provincia");       
+   $arrayProvincia= array();        
     foreach ($rows as $c){
       $aux = new Provincia($c{'idprovincia'},$c{'nombre'});
-      array_push($arrayDemo, $aux);
+      array_push($arrayProvincia, $aux);
     }
-    return $arrayDemo;        
+    return $arrayProvincia;        
   }
+
 function showProvincia($id) {
-    $rows = self::$db->getRows("SELECT * FROM provincia where idprovincia= ? ", array ("{$id}"));        
-$ObjDemo= new Demo($rows[0]{'idprovincia'},$rows[0]{'nombre'});
-    
-    return $ObjDemo;        
-  }
-function UpdateProvincia($id,$nombre) {
-    $insertrow = self::$db->updateRow("UPDATE public.provincia SET nombre = ? where idprovincia= ? ", array ("{$nombre}",$id));
+$rows = self::$db->getRows("SELECT * FROM provincia where idprovincia= ? ", array ("{$id}"));        
+$ObjProvincia= new Provincia($rows[0]{'idprovincia'},$rows[0]{'nombre'});
+  return $ObjProvincia;        
+}
+
+function updateProvincia($id,$nombre) {
+ $insertrow = self::$db->updateRow("UPDATE public.provincia SET nombre = ? where idprovincia= ? ", array ("{$nombre}",$id));
 
 }
+
 function deleteProvincia($id) {
-    $deleterow = self::$db->deleteRow("DELETE FROM public.provincia where idprovincia= ? ", array ("{$id}"));
-
+  $deleterow = self::$db->deleteRow("DELETE FROM public.provincia where idprovincia= ? ", array ("{$id}"));
 }
 
+function insertProvincia($nombre) {
+  $rows = self::$db->insertRow("INSERT INTO public.provincia(nombre) VALUES (?)", array ("{$nombre}"));             
+}
 
 }
 ?>
