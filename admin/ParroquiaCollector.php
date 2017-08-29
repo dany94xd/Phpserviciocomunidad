f233d4f19061d34cb066dcaf1c95faddf37cdb02
Olga Tohabanda <olgseb@gmail.com>
2017-08-28 19:19:22 -0500

    creación de parroquia Collector

---
+++ admin/ParroquiaCollector.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once('Parroquia.php');
+include_once('Collector.php');
+
+class ParroquiaCollector extends Collector
+{
+  
+function showParroquias() {
+   $rows = self::$db->getRows("SELECT * FROM parroquia");       
+   $arrayParroquia= array();        
+    foreach ($rows as $c){
+      $aux = new Parroquia($c{'idparroquia'},$c{'nombre'});
+      array_push($arrayParroquia, $aux);
+    }
+    return $arrayParroquia;        
+  }
+
+function showParroquia($id) {
+$rows = self::$db->getRows("SELECT * FROM parroquia where idparroquia= ? ", array ("{$id}"));        
+$ObjParroquia= new Parroquia($rows[0]{'idparroquia'},$rows[0]{'nombre'});
+  return $ObjParroquia;        
+}
+
+function updateParroquia($id,$nombre) {
+ $insertrow = self::$db->updateRow("UPDATE public.parroquia SET nombre= ? where idparroquia= ? ", array ("{$nombre}",$id));
+
+}
+
+function deleteParroquia($id) {
+  $deleterow = self::$db->deleteRow("DELETE FROM public.parroquia where idparroquia= ? ", array ("{$id}"));
+}
+
+
+function insertParroquia($nombre,$id) {
+  $rows = self::$db->insertRow("INSERT INTO public.parroquia(nombre,idciudad) VALUES (?,?)", array ("{$nombre}",$id));             
+}
+
+
+}
+?>
+
