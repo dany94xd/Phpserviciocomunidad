216523b88f843dbfc19b6887ade7862152744fb6
Olga Tohabanda <olgseb@gmail.com>
2017-09-13 21:48:02 -0500

    Cambios en css

--- admin/index.php
+++ admin/index.php
@@ -5,55 +5,35 @@ session_start();
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
-<style type="text/css">
-body{
-margin: 0px;
-}
-h1{
-font-family: 'Roboto','sans-serif';
-padding: 50px;
-background-color: #0099FF;
-color: white;
-}
-
-body{text-align: center;}
-input{
-border: 1px solid gray;
-padding: 30px;
-margin: 5px;
-}
-
-.main-button{
-padding: 20px 80px;
-color: #000;
-background-color: #0099FF;
-border: none;
-}
-</style>
+<meta charset ="utf-8">
+<title>Iniciar Sesión</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../css/admin.css" media="screen">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<header>
-</header>
 
 <aside>
 <?php
+
 if (isset($_SESSION['MiSession'])) {
-echo "<p>Hola:(".$_SESSION['MiSession'].") [<a href='salir.php'>Salir</a>]</p>";
-
-
+echo "<p>Hola:(".$_SESSION['MiSession'].") [<a id='enlaceSalir' href='salir.php'>Salir</a>]</p>";
 }
 else {
-
 session_destroy();
 }
+
 ?>
 
+<div class="iniciarSesion">
 <h1>Iniciar Sesión</h1>
-<div><img src="../images/usuario.png"></div>
+<img src="../images/usuario.png">
+</div>
 
 <form id="contact-form" action="verificarusuario.php" method= "post">
-<div style="height: 20px;"></div>
 <div class="text-fieldsl">
 <div class="float-input">
  <input name="usuario" id="usuario" type="text" placeholder="Usuario:">
@@ -70,14 +50,5 @@ session_destroy();
 </aside>
 
 
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/jquery.migrate.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script type="text/javascript" src="js/gmap3.min.js"></script>
-<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
-<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
